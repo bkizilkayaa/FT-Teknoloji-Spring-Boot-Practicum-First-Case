944aeb39dbe49438ed3d905f3d3236c77d01d8e9
Burak Kizilkaya <kizilkayaburakk@hotmail.com>
2022-09-06 16:17:42 +0300

    Initial commit

---
+++ src/main/java/com/example/springpracticum/first_case/FirstCaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpracticum.first_case;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springpracticum/first_case/FirstCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpracticum.first_case;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
